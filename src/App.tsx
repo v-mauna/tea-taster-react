ad6f90510cf1a31496f490b1d8002b191d59047c
Eric Horodyski <horodyski@ionic.io>
2020-10-12 09:37:37 -0400

    step(five): mock the UI

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Redirect, Route } from 'react-router-dom';
 import { Plugins } from '@capacitor/core';
 import { IonApp, IonRouterOutlet, isPlatform } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import Home from './pages/Home';
+import TeaPage from './tea/TeaPage';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -34,8 +34,8 @@ const App: React.FC = () => {
     <IonApp>
       <IonReactRouter>
         <IonRouterOutlet>
-          <Route path="/home" component={Home} exact={true} />
-          <Route exact path="/" render={() => <Redirect to="/home" />} />
+          <Route path="/tea" component={TeaPage} exact={true} />
+          <Route exact path="/" render={() => <Redirect to="/tea" />} />
         </IonRouterOutlet>
       </IonReactRouter>
     </IonApp>
